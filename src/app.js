9edcb7874508f2bc07b451a26c711c9c9d6a32f9
xujj <939160741@qq.com>
2019-04-25 21:01:48 +0800

    使用 chi.spies 监听回调函数

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ new Vue({
 
 // 单元测试
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+
 const expect = chai.expect
 // 测 icon
 {
@@ -81,6 +84,26 @@ const expect = chai.expect
   let svg = vm.$el.querySelector('svg')
   let {order} = window.getComputedStyle(svg)
   expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+// 测 点击
+{
+  // mock
+  const Constuctor = Vue.extend(Button)
+  const vm = new Constuctor({
+    propsData: {
+      icon: 'shezhi'
+    }
+  })
+  vm.$mount()
+  let spy = chai.spy(function () {})
+  vm.$on('click', spy)
+  // 希望这个函数被执行
+  let button = vm.$el
+  button.click()
+  expect(spy).to.have.been.called()
   vm.$el.remove()
   vm.$destroy()
 }
