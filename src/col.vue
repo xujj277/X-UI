fd2e49ddc5600a517e8cad67bf048b030e13a0e6
xujj <939160741@qq.com>
2019-05-13 19:42:57 +0800

    添加 row 的 align 属性

--- src/col.vue
+++ src/col.vue
@@ -36,8 +36,6 @@
 
 <style scoped lang="scss">
   .col {
-    width: 50%;
-
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -10,6 +10,12 @@
     props: {
       gutter: {
         type: [Number, String]
+      },
+      align: {
+        type: String,
+        validator (value) {
+          return ['left', 'right', 'center'].includes(value)
+        }
       }
     },
     mounted () {
@@ -23,6 +29,10 @@
         return {
           marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'
         }
+      },
+      rowClass () {
+        let {align} = this
+        return [align && `align-${align}`]
       }
     }
   }
@@ -31,5 +41,14 @@
 <style scoped lang="scss">
   .row {
     display: flex;
+    &.align-left {
+      justify-content: flex-start;
+    }
+    &.align-right {
+      justify-content: flex-end;
+    }
+    &.align-center {
+      justify-content: center;
+    }
   }
 </style>
