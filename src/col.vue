4c72c10896fdcd2bf8e08317e341d3b970a8449d
xujj <939160741@qq.com>
2019-05-13 16:21:35 +0800

    重构

--- src/col.vue
+++ src/col.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}"
-  >
-    <div style="border: 1px solid green; height: 100px">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -18,6 +14,21 @@
     data () {
       return {
         gutter: 0
+      }
+    },
+    computed: {
+      colClass () {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle () {
+        return {
+          paddingLeft: this.gutter/2 + 'px',
+          paddingRight: this.gutter/2 + 'px'
+        }
       }
     }
   }
