14e15079c9dee53a2d64c3980af81165513c21f3
xujj <939160741@qq.com>
2019-05-15 16:06:16 +0800

    添加 layout content header footer sider 组件

---
+++ src/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'xLayout'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
