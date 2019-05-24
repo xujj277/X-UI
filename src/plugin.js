51ef65beb5579a4741b39d87d985aff0c15e0382
xujj <939160741@qq.com>
2019-05-24 15:55:37 +0800

    解决 currentToast 的一个 bug

--- src/plugin.js
+++ src/plugin.js
@@ -8,16 +8,24 @@ export default {
       if (currentToast) {  // 防止出现两个一样的 toast
         currentToast.close()
       }
-      currentToast = createToast({Vue, message, propsData: toastOptions})  // 新建一个函数
+      currentToast = createToast({
+        Vue,
+        message,
+        propsData: toastOptions,
+        onClose: () => {
+          currentToast = null
+        }
+      })  // 新建一个函数
     }
   }
 }
 // helper
-function createToast ({Vue, message, propsData}) {
+function createToast ({Vue, message, propsData, onClose}) {
   let Constructor = Vue.extend(Toast)
   let toast = new Constructor({propsData})
   toast.$slots.default = [message]
   toast.$mount()
+  toast.$on('beforeClose', onClose)
   document.body.appendChild(toast.$el)
   return toast
 }
