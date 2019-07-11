c0c2b105935604c64568ff2b777803cf4cb2ed99
xujj <939160741@qq.com>
2019-07-11 15:41:53 +0800

    增加 disabled

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -26,7 +26,8 @@
     computed: {
       classes () {
         return {
-          active: this.active
+          active: this.active,
+          disabled: this.disabled
         }
       }
     },
@@ -36,7 +37,8 @@
       })
     },
     methods: {
-      xxx() {
+      onClick() {
+        if (this.disabled) return
         this.eventBus && this.eventBus.$emit('update:selected', this.name, this)
       }
     }
@@ -45,6 +47,7 @@
 
 <style lang='scss' scoped>
   $blue: blue;
+  $disabled-text-color: grey;
   .tabs-item {
     flex-shrink: 0;
     padding: 0 1em;
@@ -55,6 +58,10 @@
     &.active {
       color: $blue;
       font-weight: bold;
+    }
+    &.disabled {
+      color: $disabled-text-color;
+      cursor: not-allowed;
     }
   }
 </style>
