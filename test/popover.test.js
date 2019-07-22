7dc3da0a98ca701602761f71cf9e74540b8656a1
xujj <939160741@qq.com>
2019-07-22 10:43:30 +0800

    有一个测试用例不知道如何实现，先跳过

--- test/popover.test.js
+++ test/popover.test.js
@@ -24,13 +24,37 @@ describe('Button', () => {
     const vm = new Vue({
       el: div
     })
+    vm.$el.querySelector('button').click()
     vm.$nextTick(() => {
-      vm.$el.querySelector('button').click()
+      const {contentWrapper} = vm.$refs.a.$refs
+      expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+      done()
+    })
+  })
+  xit('可以设置 trigger', (done) => {
+    Vue.component('x-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <x-popover trigger="hover" ref="a">
+        <template slot="content">
+          弹出内容
+        </template>
+        <button>点我</button>
+      </x-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    setTimeout(() => {
+      let event = new Event('mouseenter')
+      vm.$el.dispatchEvent(event)
       vm.$nextTick(() => {
         const {contentWrapper} = vm.$refs.a.$refs
-        expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+        console.log(vm.$refs.a.$refs)
+        expect(contentWrapper).to.exist
         done()
       })
-    })
+    }, 200)
   })
 })
