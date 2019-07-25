88264787d5281e24d3aa2875fa9d6d31f7bb7c18
xujj <939160741@qq.com>
2019-07-25 14:48:58 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="collapse-item" @click="open = !open">
+  <div class="collapse-item" @click="toggle">
     <div class="title">
       {{title}}
     </div>
@@ -21,6 +21,27 @@
     data () {
       return {
         open: false
+      }
+    },
+    inject: ['eventBus'],
+    mounted () {
+      this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+        if (vm !== this) {
+          this.close()
+        }
+      })
+    },
+    methods: {
+      toggle() {
+        if (this.open) {
+          this.open = false
+        } else {
+          this.open = true
+          this.eventBus && this.eventBus.$emit('update:selected', this)
+        }
+      },
+      close() {
+        this.open = false
       }
     }
   }
@@ -47,7 +68,7 @@
       }
     }
     &:last-child {
-      > .title {
+      > .title:last-child {
         border-bottom-left-radius: $border-radius;
         border-bottom-right-radius: $border-radius;
       }
