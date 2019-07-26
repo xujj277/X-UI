8e7596d0a688a11539d8cc0c6c9425acd115bbb4
xujj <939160741@qq.com>
2019-07-26 09:35:23 +0800

    可以设置默认selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -16,6 +16,10 @@
       title: {
         type: String,
         require: true
+      },
+      name: {
+        type: String,
+        require: true
       }
     },
     data () {
@@ -25,9 +29,12 @@
     },
     inject: ['eventBus'],
     mounted () {
-      this.eventBus && this.eventBus.$on('update:selected', (vm) => {
-        if (vm !== this) {
+      this.eventBus && this.eventBus.$on('update:selected', (name) => {
+        console.log('name', name)
+        if (name !== this.name) {
           this.close()
+        } else {
+          this.show()
         }
       })
     },
@@ -36,12 +43,14 @@
         if (this.open) {
           this.open = false
         } else {
-          this.open = true
-          this.eventBus && this.eventBus.$emit('update:selected', this)
+          this.eventBus && this.eventBus.$emit('update:selected', this.name)
         }
       },
       close() {
         this.open = false
+      },
+      show() {
+        this.open = true
       }
     }
   }
