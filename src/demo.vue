a2841f68239f679d45f8ad0a8e1d6caa0002d021
xujj <939160741@qq.com>
2019-08-25 18:07:11 +0800

    feat: 换成vue-cli

---
+++ src/demo.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <x-cascader :source="source"></x-cascader>
+  </div>
+</template>
+
+<script>
+  import Button from './button'
+  import Cascader from './cascader'
+  export default {
+    name: '',
+    components: {
+      'x-button': Button,
+      'x-cascader': Cascader
+    },
+    data() {
+      return {
+        source: [{
+          name: '浙江',
+          children: [
+            {
+              name: '杭州',
+              children: [
+                {name: '上城'},
+                {name: '下城'},
+                {name: '江干'}
+              ]
+            },
+            {
+              name: '嘉兴',
+              children: [
+                {name: '南湖区'},
+                {name: '秀洲区'},
+                {name: '嘉善县'}
+              ]
+            }
+          ]
+        },{
+          name: '福建',
+          children: [
+            {
+              name: '厦门市',
+              children: [
+                {name: '思明区'},
+                {name: '海沧区'},
+                {name: '湖里区'}
+              ]
+            }
+          ]
+        }, {
+          name: '安徽',
+          children: [
+            {
+              name: '合肥',
+              children: [
+                {name: '卢阳'},
+                {name: '珠海'}
+              ]
+            }
+          ]
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    --button-height: 32px;
+    --font-size: 14px;
+    --button-bg: #fff;
+    --button-active-bg: #eee;
+    --border-radius: 4px;
+    --color: #999;
+    --border-color: #999;
+    --border-color-hover: #666;
+  }
+
+  #app {
+  }
+
+  body {
+    font-size: var(--font-size);
+  }
+  .box {
+    margin: 20px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import demo from "./demo.vue";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(demo)
+}).$mount("#app");
