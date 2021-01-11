fc029b14f95045bcb8d3c37b4cc332dbc23ba31e
xujj <939160741@qq.com>
2021-01-11 15:01:32 +0800

    feat(xjj): vue cli test coverage 覆盖

--- src/demo.vue
+++ src/demo.vue
@@ -1,36 +1,15 @@
 <template>
-  <div style="display:flex; justify-content:center;">
-    <div style="width: 600px; border: 1px solid black;">
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <x-sticky :distance="10">
-        <div style="border: 1px solid red">
-          我是粘粘内容
-        </div>
-      </x-sticky>
-      <p>段落1</p>
-      <p>段落2</p>
-      <p>段落3</p>
-      <p>段落4</p>
-      <p>段落5</p>
-      <p>段落6</p>
-      <p>段落7</p>
-      <p>段落8</p>
-      <p>段落9</p>
-    </div>
+  <div>
+    <x-table ></x-table>
   </div>
 </template>
 
 <script>
-  import xSticky from './sticky'
+  import xTable from './table'
 
   export default {
     name: 'demo',
-    components: { xSticky },
+    components: { xTable },
     data() {
       return {
       }
