ac6392d262ba7dcbb5c9fc948ddc1cb29a6f08a7
徐金俊 <939160741@qq.com>
2021-05-27 15:33:38 +0800

    feat(xjj): 完成switch

--- src/demo.vue
+++ src/demo.vue
@@ -1,108 +1,22 @@
 <template>
-  <div class="box-wrapper" ref="list" @scroll="scrollEvent($event)">
-    <div :style="{height: this.listHeight + 'px'}">
-      <div :style="{transform: getTransform}">
-        <p class="box" v-for="item in visibleData">{{ item.name }}</p>
-      </div>
-    </div>
+  <div>
+    <x-switch :value.sync="y"></x-switch>
   </div>
 </template>
 
-<style>
-.page {
-  display: flex;
-  justify-content: center;
-}
- .parent {
-   height: 400px;
-   overflow: hidden;
-   width: 400px;
-   border: 5px solid red;
-   box-sizing: border-box;
- }
- .child {
-   border: 5px solid green;
-   box-sizing: border-box;
- }
- 
- .hide {
-   opacity: 0.2;
- }
-</style>
-
 <script>
+import XSwitch from './switch'
 export default {
   name: 'demo',
+  components: {
+    XSwitch
+  },
   data() {
-    let array = [
-      {name: 'xujinjun', score: 100},
-      {name: 'xjj', score: 99},
-      {name: 'xujin', score: 22},
-      {name: 'x', score: 3},
-      {name: 'xujun', score: 44},
-      {name: 'xj', score: 55},
-      {name: 'jinjun', score: 66},
-      {name: 'jj', score: 88},
-      {name: 'xujinjun', score: 100},
-      {name: 'xjj', score: 99},
-      {name: 'xujin', score: 22},
-      {name: 'x', score: 3},
-      {name: 'xujun', score: 44},
-      {name: 'xj', score: 55},
-      {name: 'jinjun', score: 66},
-      {name: 'jj', score: 88},
-      {name: 'x', score: 3},
-      {name: 'xujun', score: 44},
-      {name: 'xj', score: 55},
-      {name: 'jinjun', score: 66},
-    ]
-    let bigArray = []
-    for (let i = 0; i < 5; i++) {
-      bigArray.push(...array.map((item, index) => {
-        return {id: i * 20 + index, ...item}
-      }))
-    }
     return {
-      currentPage: 1,
-      selected: [],
-      orderBy: {
-        // name: 'dsc',
-        score: ''
-      },
-      dataSource: [...bigArray],
-      loading: false,
-      screenHeight: 400,
-      itemHeight: 40,
-      start: 0,
-      end: undefined,
-      startOffset: 0,
+      y: false
     }
-  },
-  computed: {
-    listHeight () {
-      return this.dataSource.length * this.itemHeight
-    },
-    visibleCount () {
-      return Math.ceil(this.screenHeight / this.itemHeight) + 1
-    },
-    visibleData () {
-      return this.dataSource.slice(this.start, Math.min(this.end, this.dataSource.length))
-    },
-    getTransform () {
-      return `translate3d(0, ${this.startOffset}px, 0)`
-    }
-  },
-  mounted () {
-    console.log(1111, this.listHeight, this.visibleCount)
-    this.end = this.start + this.visibleCount;
   },
   methods: {
-    scrollEvent (e) {
-      let scrollTop = this.$refs.list.scrollTop
-      this.start = Math.floor(scrollTop / this.itemHeight)
-      this.end = this.start + this.visibleCount
-      this.startOffset = scrollTop - (scrollTop % this.itemHeight)
-    }
   }
 }
 </script>
@@ -118,22 +32,5 @@ html {
 }
 body {
   font-size: var(--font-size);
-}
-</style>
-
-<style lang="scss">
-.box-wrapper {
-  width: 400px;
-  height: 400px;
-  overflow: auto;
-  border: 1px solid rebeccapurple;
-  margin: auto;
-  .box {
-    height: 40px;
-    //background-color: lightblue;
-  }
-  .box:nth-child(2n) {
-    //background-color: lightgoldenrodyellow;
-  }
 }
 </style>
---
+++ src/switch.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <button :class="{checked: value}" @click="toggle"><span></span></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'xSwitch',
+  props: {
+    value: {
+      type: Boolean
+    }
+  },
+  methods: {
+    toggle () {
+      this.$emit('update:value', !this.value)
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import 'var';
+$h2: $button-height - 4px;
+button {
+  height: $button-height;
+  width: $button-height*2;
+  border: none;
+  background: $grey;
+  border-radius: $button-height/2;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: #fff;
+  border-radius: $h2 / 2;
+  transition: left 250ms;
+}
+
+button.checked {
+  background: $blue;
+}
+
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
+}
+</style>
