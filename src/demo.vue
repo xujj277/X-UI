17f84590095582b1351e6a9b05d45a837956ea16
徐金俊 <939160741@qq.com>
2021-05-31 10:47:35 +0800

    feat(xjj): switch 功能完善

--- src/demo.vue
+++ src/demo.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <x-switch :value.sync="y"></x-switch>
+    <x-switch :value.sync="y1" active-color="#13ce66" inactive-color="grey"/>
+    <x-switch :value.sync="y2" active-color="#13ce66" inactive-color="#ff4949"/>
+    <x-switch disabled :value.sync="y3" active-color="#13ce66" inactive-color="#ff4949"/>
+    <x-switch is-wave-animation :value.sync="y4" active-color="#13ce66" inactive-color="#ff4949"/>
   </div>
 </template>
 
@@ -13,7 +17,11 @@ export default {
   },
   data() {
     return {
-      y: false
+      y: false,
+      y1: false,
+      y2: true,
+      y3: false,
+      y4: false
     }
   },
   methods: {
--- src/switch.vue
+++ src/switch.vue
@@ -1,20 +1,84 @@
 <template>
   <div>
-    <button :class="{checked: value}" @click="toggle"><span></span></button>
+    <button class="x-switch"
+            :class="{'x-checked': value, 'x-switch-wave': isWaveShow}"
+            @click="toggle"
+            :disabled="disabled"
+            :style="{background: bgColor}"
+    >
+      <span></span>
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'xSwitch',
+  data () {
+    return {
+      isWaveShow: false,
+      waveFn: undefined
+    }
+  },
   props: {
     value: {
       type: Boolean
+    },
+    activeColor: {
+      type: String,
+      default: 'blue'
+    },
+    inactiveColor: {
+      type: String,
+      default: 'grey'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    isWaveAnimation: {
+      type: Boolean,
+      default: false
     }
+  },
+  mounted () {
+    this.waveFn = this.createWaveAnimation(this.isWaveAnimation)
+    this.waveFn.onCreate()
   },
   methods: {
     toggle () {
       this.$emit('update:value', !this.value)
+      this.waveFn.onToggle()
+    },
+    createWaveAnimation (isWaveAnimation) {
+      const _this = this
+      if (!isWaveAnimation) {
+        return {
+          onCreate() {},
+          onToggle() {},
+        }
+      }
+      let style = document.createElement('style')
+      return {
+        onCreate () {
+          document.getElementsByTagName('head')[0].appendChild(style)
+        },
+        onToggle () {
+          _this.isWaveShow = true
+          let keyframes = `
+            @keyframes x-switch-pulse {\
+              0% {\
+                box-shadow: 0 0 0 0 ${!_this.value ? _this.activeColor : _this.inactiveColor};\
+              }\
+            }`
+          style.innerHTML = keyframes
+        }
+      }
+    }
+  },
+  computed: {
+    bgColor () {
+      return this.value ? this.activeColor : this.inactiveColor
     }
   }
 }
@@ -23,35 +87,68 @@ export default {
 <style lang='scss' scoped>
 @import 'var';
 $h2: $button-height - 4px;
-button {
+.x-switch {
   height: $button-height;
   width: $button-height*2;
   border: none;
   background: $grey;
   border-radius: $button-height/2;
   position: relative;
-}
+  cursor: pointer;
+  
+  &-wave {
+    &::before {
+      height: $button-height;
+      width: $button-height*2;
+      border: none;
+      border-radius: $button-height/2;
+      position: absolute;
+      content: '';
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
 
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: #fff;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
-}
+      box-shadow: 0 0 0 5px transparent;
+      animation: x-switch-pulse 1s;
+    }
 
-button.checked {
-  background: $blue;
-}
+    &:active::before {
+      animation: none;
+    }
+  }
+  
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: .6;
+  }
 
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 250ms;
+  }
 
-button:focus {
-  outline: none;
+  &.x-checked > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background: transparent;
+    box-shadow: 0 0 5px 2.5px hsla(190, 20%, 91%, 0.4);
+  }
+
+  & + & {
+    margin-left: 8px;
+  }
 }
 </style>
