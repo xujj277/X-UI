f5ad1eb9896345de2e8f21ecd492295bf85ea291
徐金俊 <939160741@qq.com>
2021-06-04 17:49:09 +0800

    feat(xjj): button 基本样式完成 添加 theme size disabled 属性

--- src/button/button.vue
+++ src/button/button.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="x-button" :class="{[`icon-${iconPosition}`]: iconPosition}"
-          @click="$emit('click')">
+  <button class="x-button" :class="[`icon-${iconPosition}`, `x-theme-${theme}`, `x-size-${size}`]"
+          @click="$emit('click')" :disabled="disabled">
     <x-icon class="icon" :name="icon" v-if="!loading && icon"></x-icon>
     <x-icon name="loading" class="icon loading" v-if="loading"></x-icon>
     <span class="content"><slot></slot></span>
@@ -28,12 +28,27 @@ export default {
         return value === 'left' || value === 'right'
       },
     },
+    theme: {
+      type: String,
+      default: 'button',
+    },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
 @import "var";
+
+$active-color: #3a8ee6;
+$focus-danger-red: #ff7875;
 
 .x-button {
   font-size: $font-size;
@@ -46,17 +61,121 @@ export default {
   align-items: center;
   justify-content: center;
   vertical-align: middle;
+  cursor: pointer;
 
-  &:hover {
-    border-color: $border-color-hover;
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: #c6e2ff;
+    background: #eef5fe;
   }
 
   &:active {
-    background-color: $button-active-bg;
+    color: $active-color;
+    border-color: $active-color;
+    outline: none;
   }
 
   &:focus {
     outline: none;
+  }
+
+  &.x-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.x-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.x-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.x-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.x-theme-primary {
+    background: #579ef8;
+    color: #fff;
+    border-color: #409eff;
+
+    &:hover,
+    &:focus {
+      background: #66b1ff;
+      border-color: #66b1ff;
+    }
+
+    &:active {
+      background: $active-color;
+      border-color: $active-color;
+      color: #fff;
+    }
+    &[disabled] {
+      color: #fff;
+      background-color: #a0cfff;
+      border-color: #a0cfff;
+    }
+  }
+
+  &.x-theme-danger {
+    background: $red;
+    border-color: $red;
+    color: white;
+
+    &:hover,
+    &:focus {
+      background: $focus-danger-red;
+      border-color: $focus-danger-red;
+    }
+
+    &:active {
+      background: #dd6161;
+      border-color: #dd6161;
+    }
+    &[disabled] {
+      color: white;
+      background-color: #fab6b6;
+      border-color: #fab6b6;
+    }
+  }
+  
+  &.x-theme-button {
+    &[disabled] {
+      background-color: #fff;
+      border-color: #ebeef5;
+    }
+  }
+
+  &.x-theme-link, &.x-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: grey;
+      background-color: transparent;
+    }
+  }
+
+  &[disabled] {
+    cursor: not-allowed;
+    color: grey;
   }
 
   > .icon {
--- src/demo.vue
+++ src/demo.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <x-button @click="onClick" icon="loading" icon-position="right" loading>按钮</x-button>
+    <x-button @click="onClick" theme="primary">按钮</x-button>
+    <x-button @click="onClick" theme="danger">按钮</x-button>
+    <x-button @click="onClick" theme="text">按钮</x-button>
+    <x-button @click="onClick" theme="link">按钮</x-button>
+    <x-button @click="onClick" size="big">按钮</x-button>
+    <x-button @click="onClick" size="small">按钮</x-button>
+    <x-button @click="onClick" disabled>按钮</x-button>
+    <x-button @click="onClick" theme="primary" size="big" disabled>按钮</x-button>
+    <x-button @click="onClick" theme="danger" disabled>按钮</x-button>
+    <x-button @click="onClick" theme="link" disabled>按钮</x-button>
+    <x-button @click="onClick" theme="text" disabled>按钮</x-button>
   </div>
 </template>
 
