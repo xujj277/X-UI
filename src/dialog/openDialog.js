50ffd06f4f2e61aaf4a299992c5f0c07b90b52ce
徐金俊 <939160741@qq.com>
2021-06-08 22:20:23 +0800

    feat(xjj): 先暂时放着

--- src/dialog/openDialog.js
+++ src/dialog/openDialog.js
@@ -1,22 +1,33 @@
-import Vue from "vue";
 import Dialog from './dialog'
+let currentDialog
 
-export const openDialog = (options) => {
-  const {title, content, ok, cancel, closeOnClickOverlay} = options
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const app = new Vue({
-    render: (h) => h(Dialog, {
-      visible: true,
-      'update:visible': (val) => {
-        if (val === false) {
-          div.remove()
+export default {
+  install (Vue, options) {
+    Vue.prototype.$dialog = function (title, content, dialogOptions) {
+      if (currentDialog) {  // 防止出现两个一样的 dialog
+        currentDialog.close()
+      }
+      currentDialog = createDialog({
+        Vue,
+        title,
+        content,
+        propsData: dialogOptions,
+        onClose: () => {
+          currentDialog = null
         }
-      },
-      ok, cancel, closeOnClickOverlay
-    }, {
-      title, content
-    })
-  })
-  app.$mount(div)
+      })  // 新建一个函数
+    }
+  }
+}
+
+// helper 动态创建组件，生成一个 Dialog 组件，然后放到页面上
+function createDialog ({Vue, title, content, propsData, onClose}) {
+  let Constructor = Vue.extend(Dialog)
+  let dialog = new Constructor({propsData})
+  dialog.$slots.title = [title]
+  dialog.$slots.content = [content]
+  dialog.$mount()
+  dialog.$on('close', onClose)
+  document.body.appendChild(dialog.$el)
+  return dialog
 }
